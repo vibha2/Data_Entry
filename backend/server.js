f608e3b1f4a002085053cc8b03b588f8562dc660
vibha2 <juhisahu2203@gmail.com>
2024-01-21 04:48:44 +0530

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("server started at port 3000");
+app.listen(4000, ()=>{
+    console.log("server started at port 4000");
 });
